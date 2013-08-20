6c0e6ca00f85919f89ea9a6f92fbdbc404f5c603
Mike Redrobe <mike@redrobe.net>
2013-08-20 13:02:35 +0100

    Create input.s

---
+++ input.s
@@ -0,0 +1,25 @@
+.global _start
+_start:
+
+_read:
+  mov R7, #3 @ syscall fro read
+  mov R0, #0 @ keybaord
+  mov R2, #7 @ read 1st 5 characters
+  ldr R1,=string
+  add R1,R1, #6 @ add after "Hello "
+  swi 0
+  
+_print:
+  mov R7, #4 @ syscall for print
+  mov R0, #1 @ use monitor as stdout
+  mov R2, #12 @ length of string
+  ldr R1, =string
+  swi 0
+  
+_exit:
+  mov R7, #1
+  swi 0
+  
+.data
+string:
+  .ascii "Hello World\n"
