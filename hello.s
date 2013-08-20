4223592bfb19be61221bf684dec37d06422a3836
Mike Redrobe <mike@redrobe.net>
2013-08-20 12:47:44 +0100

    Create hello.s

---
+++ hello.s
@@ -0,0 +1,16 @@
+.global _Start
+
+_start:
+  mov R7, #4 @ syscall for print
+  mov R0, #1 @ use monitor as stdout
+  mov R2, #12 @ length of string
+  ldr R1, =string
+  swi 0
+  
+_exit:
+  move R7, #1
+  swi 0
+  
+.data
+string:
+  .ascii "Hello World\n"
